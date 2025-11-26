a9a81d4f915b725e7a365286c74290dfa8e52124
Dikshya Soni <dikshyasoni938@gmail.com>
2025-11-26 14:15:16 +0545

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -7,15 +7,28 @@ const app = express();
 // request handler
 
 
+// app.use("/hello",(req,res) =>{
+//     res.send("hello 3000");
+    
+// });
+app.get("/test",(req,res) =>{
+    res.send({firstname: "dikshya" , secondname: "soni"});
+    
+});
+app.post("/test",(req,res) =>{
+    res.send("succestfully posted");
+});
+app.delete("/test",(req,res) =>{
+    res.send("seuccesfully deleted");
+});
+// app.use("/test/2",(req,res) =>{
+//     res.send(" server 4000");
+    
+// });
 
-app.use("/hello",(req,res) =>{
-    res.send("hello 3000")
-    
-});
-app.use("/test",(req,res) =>{
-    res.send("hello from the server 4000")
-    
-});
+// app.use("/", (req, res) => {
+//   res.send("Home");
+// });
 app.listen(4000 , ()=>{
-    console.log("succesfuly created a server on port 4000")
+    console.log("succesfuly created a server on port 4000");
 });
