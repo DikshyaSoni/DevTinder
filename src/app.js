1afe21e5240dbb756a4242f20f24c66c36b100f2
Dikshya Soni <dikshyasoni938@gmail.com>
2025-11-27 13:45:52 +0545

    error handling

--- src/app.js
+++ src/app.js
@@ -2,14 +2,32 @@
 const express= require ('express')
 
 const app = express();
-const {adminAuth} = require ("../middleware/auth");
 
-app.use ("/admin",adminAuth);
 
-app.use ("/admin",adminAuth,(req,res,next) => {
-    console.log("request handler");
-    res.send("response  2");
+
+app .use("/",(err,req,res,next) => {
+    if(err){
+        res.send("soething went wrong")
+    }
 });
+app.get("/admin",(req,res) => {
+    // try{
+        throw new Error("dhgj");
+
+        res.send("admin data");
+    // }
+    // catch(err){
+// res.status(401).send("some error  contact support team");
+    // }
+
+    
+});
+app .use("/",(err,req,res,next) => {
+    if(err){
+        res.send("soething wrong")
+    }
+});
+
 app.listen(4000 , ()=>{
     console.log("succesfuly created a server on port 4000");
 });
