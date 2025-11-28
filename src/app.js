52700ec974d2e7cefb307b3f567d925d53e83414
Dikshya Soni <dikshyasoni938@gmail.com>
2025-11-28 18:26:37 +0545

    dynamic signup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 
-const express= require("express");
+const express = require("express");
 const app = express();
 
 const connectdb = require("./config/database");
@@ -8,29 +8,30 @@ const User = require("./models/user");
 
 
 app.use(express.json());
-app.post("/signup", async(req,res) => {
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
   // creating a new instnce of a user model
-res.send(req.body);
-  try{
-
+  const user = new User(req.body);
+  try {
     await user.save();
     res.send("user signup succesfull");
   }
-  catch(err){
-res.status(401).send("error saving user");
+  catch (err) {
+    res.status(401).send("error saving user" + err.message);
   }
 });
 
+
 // connectdb();
 connectdb()
-.then( ()=>{
+  .then(() => {
     console.log("database connected succesfully");
-    
-    app.listen(4000 , () => {
-        console.log("succesfuly created a server on port 4000");
+
+    app.listen(4000, () => {
+      console.log("succesfuly created a server on port 4000");
     });
-})
-.catch(err => {
+  })
+  .catch(err => {
     console.error("database cannot be connected");
-    
-});
+
+  });
