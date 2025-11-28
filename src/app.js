d5121882b41d3d4976f32433dd904e17eb57e3e8
Dikshya Soni <dikshyasoni938@gmail.com>
2025-11-28 19:17:38 +0545

    find one /find by email / find feed

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const app = express();
 const connectdb = require("./config/database");
 
 const User = require("./models/user");
+const user = require("./models/user");
 
 
 app.use(express.json());
@@ -13,6 +14,8 @@ app.post("/signup", async (req, res) => {
   // creating a new instnce of a user model
   const user = new User(req.body);
   try {
+
+
     await user.save();
     res.send("user signup succesfull");
   }
@@ -20,9 +23,48 @@ app.post("/signup", async (req, res) => {
     res.status(401).send("error saving user" + err.message);
   }
 });
+// get user by email
+
+app.get ("/user", async (req,res) => {
+  const userEmail = req.body.emailId;
+  try{
+    const user = await User.findOne({emailId:userEmail});
+  
+  if(!user){
+    res.status(400).send("user not found");
+  }else{
+    res.send(user);
+  }
+}
+  // try{
+  //    const users = await User.find({emailId:userEmail});
+  //    if(users.length == 0){
+  //     res.status(400).send("user not found");
+  //    }
+  //    else{
+
+  //      res.send(users);
+  //    }
+
+  
+  catch(err){
+    res.status(400).send("something got wrong");
+  }
+  
+});
+
+// FEED API - GET/feed -  get all the users from the database
+app.get("/feed",async (req,res) => {
+  try{
+const users = await user.find({})
+res.send(users)
+  }
+    catch (err) {
+    res.status(401).send("error saving user" + err.message);
+  }
+});
 
 
-// connectdb();
 connectdb()
   .then(() => {
     console.log("database connected succesfully");
--- src/models/user.js
+++ src/models/user.js
@@ -3,23 +3,22 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: true,
+        
     },
 
     lastName: {
         type: String,
-        required: true,
+       
     },
 
     emailId: {
         type: String,
-        required: true,
-        unique: true,
+     
     },
 
     password: {
         type: String,
-        required: true,
+        
     },
 });
 
