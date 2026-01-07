8e385ad4ac904538908e933a79642397fa41a4ba
DikshyaSoni <dikshyasoni938@gmail.com>
2026-01-07 15:11:37 +0545

    solving bug

--- src/Routes/user.js
+++ src/Routes/user.js
@@ -5,14 +5,13 @@ const ConnectionRequest = require("../models/connectionRequest");
 const User = require("../models/user");
 const mongoose = require("mongoose"); 
 
+const Safe_data = "firstName lastName Gender Age";
+
 // get all the pending connection request of the logged in user
 userRouter.get("/requests/received",userAuth ,  async (req,res) => {
 try{
 
     const loggedInUser = req.user._id;
-
-    console.log("req.user:", req.user);
-console.log("typeof req.user._id:", typeof req.user._id);
 
 const receivedRequest = await ConnectionRequest.find({
     toUserId : loggedInUser,
@@ -23,7 +22,7 @@ const receivedRequest = await ConnectionRequest.find({
 // populate("fromUserId",["firstName","lastName"]);
 
 res.json({message: "connection fetched",
-    data : receivedRequest,
+    data: receivedRequest
 });
 
 }
@@ -32,6 +31,37 @@ catch(err){
 }
 
 });
+
+userRouter.get("/user/connections",userAuth, async (req,res) => {
+    try{
+    const loggedInUser = req.user._id;
+    const allConnections = await ConnectionRequest.find({
+        $or:[
+            {toUserId : loggedInUser ,status: "accepted"},
+            {fromUserId : loggedInUser, status : "accepted"}
+            
+        ],
+    }).populate("fromUserId",Safe_data)
+    .populate("toUserId",Safe_data);
+
+    const data = allConnections.map((row) => {
+if(row.fromUserId._id.toString() == loggedInUser.toString()){
+    return row.toUserId;
+}
+return row.fromUserId;
+});
+       
+    res.json({data:data});
+}
+catch(err){
+    res.status(400).send({mesage : err.message})
+}
+});
+
+
+
+
+
 
 
 module.exports = userRouter;
